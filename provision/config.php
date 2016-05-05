30804b09c83890e88f2e5fc5f3831c413f76bbe9
wott <william.ott@medicustek.com>
2016-05-05 17:32:13 +0800

    Fix login dn

--- provision/config.php
+++ provision/config.php
@@ -252,7 +252,7 @@ $servers->setValue('server','host','127.0.0.1');
 // $servers->setValue('server','port',389);
 /* Array of base DNs of your LDAP server. Leave this blank to have phpLDAPadmin
    auto-detect it for you. */
-$servers->setValue('server','base',array('dc=medicustek,dc=com'));
+$servers->setValue('server','base',array('dc=example,dc=com'));
 /* Five options for auth_type:
    1. 'cookie': you will login via a web form, and a client-side cookie will
       store your login dn and password.
@@ -275,7 +275,7 @@ $servers->setValue('login','auth_type','session');
    auth_type, then you can also specify the bind_id/bind_pass here for searching
    the directory for users (ie, if your LDAP server does not allow anonymous
    binds. */
-$servers->setValue('login','bind_id','cn=admin,dc=medicustek,dc=com');
+$servers->setValue('login','bind_id','cn=admin,dc=example,dc=com');
 #  $servers->setValue('login','bind_id','cn=Manager,dc=example,dc=com');
 /* Your LDAP password. If you specified an empty bind_id above, this MUST also
    be blank. */
