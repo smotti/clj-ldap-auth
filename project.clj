226dd970b3b2c364628d07c7c3158fbb7fdbbeb6
wott <william.ott@medicustek.com>
2016-05-05 16:20:17 +0800

    Bump version & remove plugins and some deps

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(defproject clj-ldap-auth "0.1.1"
+(defproject clj-ldap-auth "0.2.0"
   :description "A library that provides authentication via an LDAP server"
-  :url "http://github.com/realestate-com-au/clj-ldap-auth"
+  :url "http://github.com/smotti/clj-ldap-auth"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.unboundid/unboundid-ldapsdk "2.3.3"]]
-  :plugins [[codox "0.6.1"]
-            [lein-midje "3.0.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.unboundid/unboundid-ldapsdk "3.1.1"]])
